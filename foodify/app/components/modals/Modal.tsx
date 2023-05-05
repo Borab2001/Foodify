152915679582eb97a22aae4749ec016a2bbc3df9
Bora <borabalos9@gmail.com>
2023-05-05 12:31:24 +0200

    Install Next-Auth, Prisma Client, Prisma Adapter & BCrypt

--- foodify/app/components/modals/Modal.tsx
+++ foodify/app/components/modals/Modal.tsx
@@ -68,7 +68,7 @@ const Modal: React.FC<ModalProps> = ({
 
     return (
         <>
-            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 transition-all duration-300 ease-in-out">
                 <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full md:h-auto lg:h-auto">
                     
                     {/* Content */}
