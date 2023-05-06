b01da904c76f6e74cb28d6ac8bd6f0dcdda3443d
Bora <borabalos9@gmail.com>
2023-05-06 02:27:33 +0200

    Adding Query URL

--- foodify/app/components/CategoryBox.tsx
+++ foodify/app/components/CategoryBox.tsx
@@ -1,6 +1,7 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps {
     icon: IconType;
@@ -17,11 +18,31 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
     const params = useSearchParams();
 
     const handleClick = useCallback(() => {
+        let currentQuery = {};
 
-    }, []);
+        if (params) {
+            currentQuery = qs.parse(params.toString());
+        }
+
+        const updatedquery: any = {
+            ...currentQuery,
+            category: label
+        }
+
+        if (params?.get('category') === label) {
+            delete updatedquery.category;
+        }
+
+        const url = qs.stringifyUrl({
+            url: '/',
+            query: updatedquery
+        }, { skipNull: true });
+
+        router.push(url);
+    }, [label, params, router]);
 
     return (
-        <div className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover-text-neutral-800 transition cursor-pointer 
+        <div onClick={handleClick} className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover-text-neutral-800 transition cursor-pointer 
             ${selected ? 'border-b-neutral-800' : 'border-transparent'}
             ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}>
             <Icon size={26} />
