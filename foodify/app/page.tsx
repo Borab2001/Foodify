e700723b86714d46a7ecdc44112bf2f9eaec38be
Bora <borabalos9@gmail.com>
2023-05-07 15:06:55 +0200

    Empty State

--- foodify/app/page.tsx
+++ foodify/app/page.tsx
@@ -1,8 +1,37 @@
 import Auth from "./Auth";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
+
   return (
-    //<div className="text-green text-2xl">Hello Foodify</div>
-    <Auth />
+    <ClientOnly>
+      <Container>
+        <div className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        ">
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+    // <Auth />
   )
 }
