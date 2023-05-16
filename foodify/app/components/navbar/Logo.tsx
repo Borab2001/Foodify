05dd23477c977c850fdf7536e5b5591bcb9e111a
Bora <borabalos9@gmail.com>
2023-05-17 00:33:53 +0200

    Logo adapted to dark mode

--- foodify/app/components/navbar/Logo.tsx
+++ foodify/app/components/navbar/Logo.tsx
@@ -8,7 +8,11 @@ const Logo = () => {
 
     return (
         <>
-            <Image onClick={() => router.push('/')} alt="Logo" className="block cursor-pointer" height="125" width="125" src="/images/logo.svg" />
+            <div className="flex flex-row items-center justify-center gap-4 text-2xl font-semibold text-black dark:text-white">
+                {/* <Image onClick={() => router.push('/')} alt="Logo" className="block cursor-pointer" height="125" width="125" src="/images/logo.svg" /> */}
+                <Image onClick={() => router.push('/')} alt="Logo" className="block cursor-pointer" height="35" width="35" src="/images/icon.svg" />
+                Foodify
+            </div>
         </>
     );
 }
