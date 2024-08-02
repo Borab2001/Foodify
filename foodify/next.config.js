9b079806dc93263817f8c51fb309cfb6f612d1b4
Bora <borabalos9@gmail.com>
2024-08-02 14:11:55 +0200

    Installed PWA

--- foodify/next.config.js
+++ foodify/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 const nextConfig = {
   experimental: {
     appDir: true,
@@ -12,4 +17,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
